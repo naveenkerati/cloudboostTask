6179e3beee589ab90c5077c377f9fcedd328c211
naveenk123 <naveen.kerati@securifi.com>
2017-06-06 17:54:49 +0530

    added utilities to the app

---
+++ appUtils.js
@@ -0,0 +1,13 @@
+var makeRequest = function(url){
+//TO-DO
+}
+var getResidents = function(residents){
+	var residentNames = [];
+
+}
+var sendRequest = function(url){
+	//TO-DO
+}
+module.exports.utils = {
+  makeRequest,getResidents,sendRequest
+}
